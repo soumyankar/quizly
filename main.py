c18a390ddf8a324bbb8940ef6fc4d287080c70cc
Vertigo <soumyankarm@gmail.com>
2021-06-14 12:57:47 +0530

    Added necessary libraries for postgres

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from __future__ import print_function # For printing to console. !!Must be placed on beginning of file!!
 
 from flask import Flask, Blueprint, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
 from jinja2 import TemplateNotFound
 
 # OS Libraries
@@ -10,10 +9,19 @@ import os
 
 # Import src python files
 from src.homepage import homepage
+from src.adminlogin import adminlogin, db, login_manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.register_blueprint(homepage, url_prefix="")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') # os.environ.get('DATABASE_URL') #sqlite:///test.db' # 3 forward slashes means relative path; 4 forward slashes means exact path
+app.config['SECRET_KEY'] = "123456789"
+Bootstrap(app)
+db.init_app(app)
+login_manager.init_app(app)
 
+app.register_blueprint(homepage, url_prefix="")
+app.register_blueprint(adminlogin, url_prefix="")
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
