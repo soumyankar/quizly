69d9a749d4816e127d7f0b9afa5643b2a93f8d84
rishabh0282 <rishabhtiwari0282@gmail.com>
2021-07-03 01:51:21 +0530

    Date and Time validator added

--- app/forms/quizforms.py
+++ app/forms/quizforms.py
@@ -7,11 +7,30 @@ from wtforms import StringField, PasswordField, BooleanField, SelectField, TextF
 from wtforms import validators, ValidationError
 from wtforms.validators import InputRequired, Email, Length
 from app.models.models import User
+from wtforms_components import DateRange
+from datetime import date, datetime
+
+
+today = date.today()
+current_time = datetime.now().time()
+print(current_time)
+print(today)
+
+def date_validator(form, field):
+
+    if field.data < today:
+        raise ValidationError('Past Date cannot be selected')
+
+
+def time_validator(form, field):
+    if field.data < current_time:
+        raise ValidationError('Past Time cannot be selected')
 
 class QuizRegisterForm(FlaskForm):
 
     name = StringField('Quiz Name', validators=[InputRequired()])
-    date = DateField('Quiz Date', validators=[InputRequired()])
-    time = TimeField('Time', validators=[InputRequired()])
+    date = DateField('Quiz Date', default=today , validators=[InputRequired(), date_validator])
+    time = TimeField('Time', default=current_time, validators=[InputRequired(), time_validator])
     subscription_price = IntegerField('User Registration Price', validators=[InputRequired()])
     quiz_master = SelectField('Quiz Master', choices="", validators=[InputRequired()], render_kw = {"class":"form-control"})
+
