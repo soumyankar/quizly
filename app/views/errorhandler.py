f8b2dddc06845ad97a79daff77bf54cc3ea171bb
rishabh0282 <47480158+rishabh0282@users.noreply.github.com>
2021-07-14 00:04:53 +0530

    User_profile Errorhandler

--- app/views/errorhandler.py
+++ app/views/errorhandler.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 errorhandler = Blueprint('errorhandler', __name__)
 
 
 @errorhandler.app_errorhandler(404)
 def error_404(error):
-    return render_template('errors/404.html'), 404
-
+    return render_template('errors/404.html', error=error), 404
 
 @errorhandler.app_errorhandler(403)
 def error_403(error):
