764c3755f2f8054485cca325ddc7fa1c63c4012a
rishabh0282 <47480158+rishabh0282@users.noreply.github.com>
2021-07-15 21:14:06 +0530

    DashboardIcons

--- app/navbar_manager.py
+++ app/navbar_manager.py
@@ -2,36 +2,43 @@ from flask_navigation import Navigation
 
 nav = Navigation()
 
-nav.Bar('default_navbar',[
-nav.Item('Home', 'homepage.index'),
-nav.Item('Contact', 'homepage.contact'),
-nav.Item('Quiz', 'quiz.quiz_homepage', items=[
-	nav.Item('Create Quiz', 'quiz.quiz_plans_page'),
-	nav.Item('Browse Quiz', 'quiz.quiz_browse_page'),
-]),
+nav.Bar('default_navbar', [
+    nav.Item('Home', 'homepage.index'),
+    nav.Item('Contact', 'homepage.contact'),
+    nav.Item('Quiz', 'quiz.quiz_homepage', items=[
+        nav.Item('Create Quiz', 'quiz.quiz_plans_page'),
+        nav.Item('Browse Quiz', 'quiz.quiz_browse_page'),
+    ]),
 ])
 
 nav.Bar('user_navbar', [
-	nav.Item('Dashboard', 'user_dashboard.user_dashboard_page', html_attrs={'icon-name': 'fas fa-tachometer-alt'}),
-	nav.Item('Edit Profile', 'user.edit_user_profile'),
-	nav.Item('Quiz', 'quiz.quiz_homepage', items=[
-			nav.Item('Create Quiz', 'quiz.quiz_plans_page', html_attrs={'icon-name': 'fas fa-tachometer-alt'}),
-			nav.Item('Browse Quiz', 'quiz.quiz_browse_page')
-		]),
-	nav.Item('Manage', '', items=[
-		nav.Item('Owned Quiz', 'user_dashboard.user_quiz_owned'),
-		nav.Item('Subscribed Quiz', 'user_dashboard.user_quiz_subscribed'),
-		nav.Item('Hosting Quiz', 'user_dashboard.user_quiz_master')
-		])
-])
+        nav.Item('Dashboard', 'user_dashboard.user_dashboard_page',
+                 html_attrs={'icon-name': 'fas fa-tachometer-alt'}),
+        nav.Item('Edit Profile', 'user.edit_user_profile',
+                 html_attrs={'icon-name': 'fas fa-user-edit'}),
+        nav.Item('Quiz', 'quiz.quiz_homepage',  html_attrs={'icon-name': 'fas fa-house-user'}, items=[
+            nav.Item('Create Quiz', 'quiz.quiz_plans_page',
+                     html_attrs={'icon-name': 'far fa-plus-square'}),
+            nav.Item('Browse Quiz', 'quiz.quiz_browse_page',
+                     html_attrs={'icon-name': 'fas fa-folder-open'})
+        ]),
+        nav.Item('Manage', '', items=[
+            nav.Item('Owned Quiz', 'user_dashboard.user_quiz_owned',
+                     html_attrs={'icon-name': 'fas fa-folder-open'}),
+            nav.Item('Subscribed Quiz', 'user_dashboard.user_quiz_subscribed', html_attrs={
+                     'icon-name': 'fas fa-folder-open'}),
+            nav.Item('Hosting Quiz', 'user_dashboard.user_quiz_master',
+                     html_attrs={'icon-name': 'fas fa-folder-open'})
+        ])
+        ])
 
 nav.Bar('user_settings_navbar', [
-	nav.Item('Profile', 'user.edit_user_profile'),
-	# nav.Item('Change username', 'user.change_username'),
-	nav.Item('Change password', 'user.change_password')
-])
+        nav.Item('Profile', 'user.edit_user_profile'),
+        # nav.Item('Change username', 'user.change_username'),
+        nav.Item('Change password', 'user.change_password')
+        ])
 
 nav.Bar('user_topnavbar', [
-	nav.Item('Settings', 'user.edit_user_profile'),
-	nav.Item('Sign-out', 'user.logout')
-])
+        nav.Item('Settings', 'user.edit_user_profile'),
+        nav.Item('Sign-out', 'user.logout')
+        ])
